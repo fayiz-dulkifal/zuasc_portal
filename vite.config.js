952791da17c337da33471941d44d2357b4183e7a
fayismahmood <fmthottathil@gmail.com>
2021-07-15 13:51:58 +0530

    adffsdf

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+const htmlMinifier = require("rollup-plugin-html-minifier")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  server: {
-    host: true
-  }
+  plugins: [reactRefresh(),
+    htmlMinifier({
+      collapseWhitespace: true,
+    })
+  
+  ],
+  
 })
